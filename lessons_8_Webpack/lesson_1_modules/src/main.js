13437893d9d68ef65418ccb1f9d3b2bc6a876202
Gena <mr.genag@mail.ru>
2023-02-16 14:28:18 +0500

    Добавил файл html и Скрипты

---
+++ lessons_8_Webpack/lesson_1_modules/src/main.js
@@ -0,0 +1,15 @@
+const dateCalcForm = document.getElementById("datecalc");
+const dateCalcResult = document.getElementById("datecalc__result");
+
+dateCalcForm.addEventListener("submit", handleCalcDates);
+
+function handleCalcDates(event) {
+    dateCalcResult.innerHTML = "";
+    event.preventDefault();
+
+    let { firstDate, secondDate } = event.target.elements;
+    firstDate = firstDate.value, secondDate = secondDate.value;
+
+    if (firstDate && secondDate) console.log(firstDate, secondDate);
+    else console.log("Для расчета промежутка необходимо заполнить оба поля");
+}

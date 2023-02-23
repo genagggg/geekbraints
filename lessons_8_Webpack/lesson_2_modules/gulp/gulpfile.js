d462d84c6bd1799281b7aa19c845b6b5e32f6167
Gena <mr.genag@mail.ru>
2023-02-23 20:51:20 +0500

    Инициализировал gulp

---
+++ lessons_8_Webpack/lesson_2_modules/gulp/gulpfile.js
@@ -0,0 +1,7 @@
+const { src, dest } = require('gulp')
+const sass = require('gulp-sass')
+const csso = require('gulp-csso')
+const include = require('gulp-file-include')
+const htmlmin = require('gulp-htmlmin')
+const del = require('del')
+const sync = require('browser-sync').create()

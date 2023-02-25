a9d61b371a96f40906b2b737fb23b28f33c48493
Gena <mr.genag@mail.ru>
2023-02-25 12:34:44 +0500

    Минимизировал css

--- lessons_8_Webpack/lesson_2_modules/gulp/gulpfile.js
+++ lessons_8_Webpack/lesson_2_modules/gulp/gulpfile.js
@@ -3,5 +3,29 @@ const sass = require('gulp-sass')
 const csso = require('gulp-csso')
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
-const del = require('del')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+//const del = require('del')
 const sync = require('browser-sync').create()
+
+function html() {
+    return src('src/**.html')
+        .pipe(include({
+            prefix: '@@'
+        }))
+        .pipe(dest('dist'))
+}
+
+function scss() {
+    return src('src/scss/**.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(concat('index.css'))
+        .pipe(dest('dist'))
+}
+
+exports.html = html
+exports.scss = scss
